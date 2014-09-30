800a2a124474210b4a80aedbfe27806c37b5d4da
Vi <user@Vis-MacBook-Air.local>
2014-09-30 14:47:09 -0500

    Initial Commit

---
+++ WordPlay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WordPlay
+//
+//  Created by Vi on 9/30/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WordPlay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WordPlay
+//
+//  Created by Vi on 9/30/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
